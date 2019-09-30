2425b63f83e60891aa1cc28b21d77f18a3de2685
Miles Eastwood <MEastwood@spartaglobal.com>
2019-09-30 14:30:01 +0100

    Set Expedition class. NOw need to populate it with expeditions

--- expedition_class.py
+++ expedition_class.py
@@ -1,4 +1,12 @@
 # Create expeditions with:
+class Expeditions():
+
+    def __init__(self, destination, spaceship, origin = "Gazorpazorp"):
+        self.origin = origin
+        self.destination = destination
+        self.spaceship = spaceship
+        self.__passenger_list = []
+
 # An origin (probably always Gazorpazorp),
 # Should have a destination,
 # A space craft assinged to it,
